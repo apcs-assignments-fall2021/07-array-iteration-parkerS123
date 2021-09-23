0bb9fe06a9bbb10a61214e87770226ba7a5ff09d
parkerschwantes <23pschwantes@athenian.org>
2021-09-23 10:54:21 -0700

    all good

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -42,7 +42,7 @@ public class MyMain {
         String[] dictionary = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","cat","dog","cats","dogs"};
 
         int i = 0;
-        while (i > dictionary.length){ // the loop is not being used for some reason -- skipping right to false
+        while (i < dictionary.length){ // the loop is not being used for some reason -- skipping right to false
             // System.out.println(dictionary.length);
             if (word.equals(dictionary[i])) {
                 return true;
